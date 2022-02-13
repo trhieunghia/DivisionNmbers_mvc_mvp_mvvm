c965faa1ff9cedebabe8b456f71256194f92cb4d
HieuNghia <hieunghia1905@gmail.com>
2022-02-13 19:15:07 +0700

    chuyen tu phep tinh tong sang phep tinh thuong hai so

--- app/src/main/java/com/example/sumtwonumbers_mvc_mvp_mvvm/mvp/MvpActivity.kt
+++ app/src/main/java/com/example/sumtwonumbers_mvc_mvp_mvvm/mvp/MvpActivity.kt
@@ -1,7 +1,8 @@
-package com.example.sumtwonumbers_mvc_mvp_mvvm
+package com.example.sumtwonumbers_mvc_mvp_mvvm.mvp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sumtwonumbers_mvc_mvp_mvvm.R
 
 class MvpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
